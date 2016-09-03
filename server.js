f85b65eaa134168bf0b9164905521be8149b6cf3
dfcook <dfcook@hotmail.com>
2016-09-03 12:48:05 +0100

    Use express in azure

---
+++ server.js
@@ -0,0 +1,17 @@
+var express = require('express');
+var app = express();
+
+app.get("/", function(req, res) {
+    res.sendfile("dist/index.html");
+ });
+
+ /* serves all the static files */
+ app.get(/^(.+)$/, function(req, res){ 
+     console.log('static file request : ' + req.params);
+     res.sendfile( __dirname + "/dist/" + req.params[0]); 
+ });
+
+ var port = process.env.PORT || 5000;
+ app.listen(port, function() {
+   console.log("Listening on " + port);
+ });
