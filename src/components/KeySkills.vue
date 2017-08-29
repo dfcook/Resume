f18f46bec6387860fb46eb72258f711c7e7a4af7
Daniel Cook <dfcook@hotmail.com>
2017-08-29 14:40:57 +0100

    test jenkins cd

--- src/components/KeySkills.vue
+++ src/components/KeySkills.vue
@@ -7,7 +7,7 @@
           <v-card height="100%">
             <v-card-title>
               <h5>
-                <em>Skills</em>
+                <em>Foo Skills</em>
               </h5>
               <br>              
             </v-card-title>
