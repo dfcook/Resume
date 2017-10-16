e7a04821f726ebe5bfe9ccb556176a769d4e72a8
Daniel Cook <dfcook@hotmail.com>
2017-10-16 18:14:51 +0100

    Correct api url

--- src/api/resources.ts
+++ src/api/resources.ts
@@ -1,3 +1,3 @@
 import axios from 'axios'
 
-export default function getResume() { return axios.get('http://localhost:51376/api/resume/dfcook') }
+export default function getResume() { return axios.get('http://46.101.94.208:8080/api/resume/dfcook') }
